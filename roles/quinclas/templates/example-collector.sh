7748fbc7165b90561e64f6053449b196c14c377a
thraneh <thraneh@gmail.com>
2018-01-13 22:27:10 +0100

    now possible to define the list of gateways per host in the inventory file

---
+++ roles/quinclas/templates/example-collector.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Configuration
+CONDA_DIR="/trading/infra/miniconda2"
+LOG_DIR="/trading/logs"
+SOCKET="/var/tmp/femas.sock"
+PIDFILE="/trading/infra/run/.collector.pid"
+CHDIR="/trading/infra/run/collector"
+STDOUT="/trading/capture/collector.tick"
+
+# Parse options
+while [[ $# -gt 0 ]]
+do
+key="$1"
+case $key in
+  --start)
+  TYPE="start"
+  shift
+  ;;
+  --stop)
+  TYPE="stop"
+  shift
+  ;;
+  *)
+  >&2 echo "unknown argument" && exit 1
+  ;;
+esac
+done
+
+# Daemonize
+case $TYPE in
+  start)
+  source "$CONDA_DIR/bin/activate"
+  export GLOG_log_dir="$LOG_DIR"
+  export GLOG_minloglevel=0
+  /usr/bin/daemon --respawn --pidfile "$PIDFILE" --chdir "$CHDIR" --stdout "$STDOUT" --unsafe \
+      -- "$CONDA_PREFIX/bin/example-collector" --local-address "$SOCKET"
+  ;;
+  stop)
+  /usr/bin/pkill -F "$PIDFILE" >/dev/null 2>&1
+  ;;
+esac
---
+++ roles/quinclas/templates/quinclas-gateway.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Configuration
+CONDA_DIR="/trading/infra/miniconda2"
+LOG_DIR="/trading/logs"
+CONFIG="/trading/config/{{ item }}.conf"
+SOCKET="/var/tmp/{{ item }}.sock"
+PIDFILE="/trading/infra/run/.{{ item }}.pid"
+CHDIR="/trading/infra/run/{{ item }}"
+
+# Parse options
+while [[ $# -gt 0 ]]
+do
+key="$1"
+case $key in
+  --start)
+  TYPE="start"
+  shift
+  ;;
+  --stop)
+  TYPE="stop"
+  shift
+  ;;
+  *)
+  >&2 echo "unknown argument" && exit 1
+  ;;
+esac
+done
+
+# Daemonize
+case $TYPE in
+  start)
+  source "$CONDA_DIR/bin/activate"
+  export GLOG_log_dir="$LOG_DIR"
+  export GLOG_minloglevel=0
+  /usr/bin/daemon --respawn --pidfile "$PIDFILE" --chdir "$CHDIR" --unsafe \
+      -- "$CONDA_PREFIX/bin/quinclas-{{ item }}" --config "$CONFIG" --local-address "$SOCKET" --monitor-port "{{ gateway_ports[item] }}"
+  ;;
+  stop)
+  /usr/bin/pkill -F "$PIDFILE" >/dev/null 2>&1
+  ;;
+esac
